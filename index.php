54a413948a6ebc2f30725260491dcf2cc6b46e22
Margit <margit.pilden@gmail.com>
2014-09-16 08:53:57 +0300

    Basic HTML

---
+++ index.php
@@ -0,0 +1,19 @@
+<! DOCKTYPE html>
+<html>
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+	<title>
+	harjtus-3s - PHP põhitõed
+	</title>
+</head>
+
+<body>
+<h1>Arvutamine murd- ja täisarvudega</h1>
+<?php echo $float = 3.14; ?><br> 
+<?php echo $float +7; ?><br>
+<?php echo 4/3; ?><br>
+
+
+</body>
+
+</html>
